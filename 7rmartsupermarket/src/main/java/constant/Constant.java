9aa007c639e8953a2028438e9db12f87a543c882
ACHUVISHNUKUNJU <ACHUVISHNUKUNJU@ACHUVISHNU>
2026-07-15 12:39:59 +0530

    Ninth Commit

--- 7rmartsupermarket/src/main/java/constant/Constant.java
+++ 7rmartsupermarket/src/main/java/constant/Constant.java
@@ -3,4 +3,5 @@ package constant;
 public class Constant {
 	public static final String TESTDATA=System.getProperty("user.dir")+"\\src\\test\\resources\\TestData.xlsx";
     public static final String LAYSIMG=System.getProperty("user.dir")+"\\src\\test\\resources\\Lays.jpg";
+    public static final String CONFIGFILE=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 }
--- 7rmartsupermarket/src/test/java/testscript/Base.java
+++ 7rmartsupermarket/src/test/java/testscript/Base.java
@@ -1,6 +1,9 @@
 package testscript;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,14 +13,28 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import constant.Constant;
 import utilities.ScreenShotUtility;
+import utilities.WaitUtility;
 
 public class Base {
 	public WebDriver driver;
+	Properties pr;
+	FileInputStream fileinput;
 	@BeforeMethod
 	@Parameters("browser")
 	public void initializebrowser(String Browser) throws Exception
 	{
+		try
+		{
+			pr=new Properties();
+			fileinput=new FileInputStream(Constant.CONFIGFILE);
+			pr.load(fileinput);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 		if(Browser.equalsIgnoreCase("Chrome"))
 		{
 		driver=new ChromeDriver();
@@ -30,7 +47,9 @@ public class Base {
 		{
 			throw new Exception("Invalid Browser");
 		}
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		//driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.get(pr.getProperty("url"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize();
 		
 	}
